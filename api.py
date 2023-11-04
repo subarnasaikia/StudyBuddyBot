59cfa3245d4c9f39a8acfa829aaeb71627fe6a5c
Subarna Saikia <71511449+subarnasaikia@users.noreply.github.com>
2023-11-05 02:19:53 +0530

    Update api.py

--- api.py
+++ api.py
@@ -3,5 +3,5 @@
 def getKey():
     """ OpenAI API key """
     # API_KEY = getpass("Enter your OpenAI API key: ")
-    API_KEY = "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"
+    API_KEY = ""
     return API_KEY
